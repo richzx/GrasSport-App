957e817038f45ffebb4355b010882330e089da9a
richz <richarmiguelrodriguez@gmail.com>
2023-09-13 17:39:43 -0500

    div-px

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <main id='about' className='mt-[4rem] w-full flex justify-center items-center'>
       <section className='m-4 py-9 px-5 flex items-center flex-col gap-4 border-[.2rem] border-[#1e7f1436] rounded-2xl shadow-md'>
-        <div className='flex flex-col gap-2 p-4 lg:max-w-4xl'>
+        <div className='flex flex-col gap-2 p-2 lg:max-w-4xl'>
           <h1 className='text-[1.3rem] text-app09 text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
           <p className='text-[.98rem] text-app10 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi odit magnam asperiores suscipit dicta hic temporibus fuga quasi optio repellendus. Eius deleniti odio eligendi commodi?</p>
         </div>
