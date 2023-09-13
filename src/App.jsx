eed58d41d9b7591fdd4b117f9c425e873f694c38
richz <richarmiguelrodriguez@gmail.com>
2023-09-13 17:07:03 -0500

    Close

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
       {/* About */}
       <About/>
       {/* Contact */}
-      <Contact/>
+      {/* <Contact/> */}
       {/* Footer */}
-      <Footer/>
+      {/* <Footer/> */}
     </>
   )
 }
